bb8c46a13ce38e9cf939b7fd8667b380d23abd05
gopalprojectx100 <gopal.sasmal.29.3@mountblue.tech>
2024-09-04 15:11:51 +0530

    Add problem 2 solution

--- testProblem.js
+++ testProblem.js
@@ -1,5 +1,6 @@
 import users from "./user.js";
-import { interestedInVideoGame } from "./problem.js";
+import { interestedInVideoGame, getAllUserStayInGermany } from "./problem.js";
 
-const ans = interestedInVideoGame(users);
-console.log(ans);
+// const ans = interestedInVideoGame(users);
+const ans2 = getAllUserStayInGermany(users);
+console.log(ans2);
