78b07fafac79473c07a9d62f2a31fb0bdf491a0e
gopalprojectx100 <gopal.sasmal.29.3@mountblue.tech>
2024-09-04 14:57:43 +0530

    Add problem 1 solution

---
+++ problem.js
@@ -0,0 +1,45 @@
+import users from "./user.js";
+
+// Q1 Find all users who are interested in playing video games.
+function interestedInVideoGame(users) {
+  if (!Object.keys(users).length === 0) {
+    return [];
+  }
+
+  const interestedVideoGamePlayers = [];
+  for (let user in users) {
+    const { interests, interest } = users[user];
+    if (interests && interests.length === 0) {
+      continue;
+    }
+    if (interest && interest.length === 0) {
+      continue;
+    }
+
+    const regex = /video game/i;
+    for (let index = 0; interests && index < interests.length; index++) {
+      if (regex.test(interests[index])) {
+        interestedVideoGamePlayers.push(user);
+      }
+    }
+
+    for (let index = 0; interest && index < interest.length; index++) {
+      if (regex.test(interest[index])) {
+        interestedVideoGamePlayers.push(user);
+      }
+    }
+  }
+
+  return interestedVideoGamePlayers;
+}
+
+export { interestedInVideoGame };
+
+/*
+
+Q2 Find all users staying in Germany.
+Q3 Find all users with masters Degree.
+Q4 Group users based on their Programming language mentioned in their designation.
+
+
+*/
---
+++ testProblem.js
@@ -0,0 +1,5 @@
+import users from "./user.js";
+import { interestedInVideoGame } from "./problem.js";
+
+const ans = interestedInVideoGame(users);
+console.log(ans);
